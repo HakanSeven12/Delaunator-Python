47e3b1550f41490136b8bb208a6c84c94f29cba1
Hakan Seven <hakanseven12@gmail.com>
2020-03-02 11:56:44 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,16 +2,23 @@ import numpy as np
 from Delaunator import Delaunator
 import time
 
-start_time = time.clock()
+start_time = time.process_time()
 
 #create random 2000 2d points between 4000000 and 4500000
-points = np.random.randint(4000000,4500000,size=(100000,2))
+points = np.random.randint(4000000,4500000,size=(1000,2))
 
-point_creation_time = time.clock() - start_time
+point_creation_time = time.process_time() - start_time
 
-triangles = Delaunator().run(points)
-print(triangles)
+triangles = Delaunator(points).triangles
+halfedges = Delaunator(points).halfedges
+hull = Delaunator(points).hull
+coords = Delaunator(points).coords
 
-triangulation_time = time.clock() - point_creation_time
-print(point_creation_time)
-print(triangulation_time)
+print("\ncoords = "+str(coords))
+print("\ntriangles = "+str(triangles))
+print("\nhalfedges = "+str(halfedges))
+print("\nhull = "+str(hull))
+
+triangulation_time = time.process_time() - point_creation_time
+print("\n" + str(point_creation_time))
+print("\n" + str(triangulation_time))
