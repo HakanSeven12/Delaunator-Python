ebde47a640cc4fda8ba88722ec5ced387978b1bc
Hakan Seven <hakanseven12@gmail.com>
2020-03-05 14:39:10 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,7 +5,7 @@ import time
 start_time = time.process_time()
 
 #create random 2000 2d points between 4000000 and 4500000
-points = np.random.randint(4000000,4500000,size=(1000,2))
+points = np.random.randint(1,1677.7215,size=(10000,2))
 
 point_creation_time = time.process_time() - start_time
 
